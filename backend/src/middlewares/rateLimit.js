3b1596887e73a7586fb94d74a5cc802012d9b637
ramonrp99 <ramonrodped@gmail.com>
2026-02-25 19:09:49 +0100

    [refactor] Añade error personalizado en "rateLimit.js"

--- backend/src/middlewares/rateLimit.js
+++ backend/src/middlewares/rateLimit.js
@@ -1,10 +1,13 @@
 const { rateLimit } = require('express-rate-limit')
 const config = require('../config/env')
+const AppError = require('../core/AppError')
 
 const aiLimiter = rateLimit({
     windowMs: config.rateLimit.ai.windowMs,
     limit: config.rateLimit.ai.max,
-    message: 'Se ha alcanzado límite de peticiones.'
+    handler: (req, res, next, options) => {
+        next(new AppError(429, 'Se ha alcanzado límite de peticiones. Inténtalo de nuevo más tarde.'))
+    }
 })
 
 module.exports = { aiLimiter }
