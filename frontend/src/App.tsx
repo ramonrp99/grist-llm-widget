8beda688133a14b74f8eb7e58a38c0a79b5a791d
ramonrp99 <ramonrodped@gmail.com>
2026-03-15 01:20:38 +0100

    [feat] Añade carrusel de opciones predefinidas
    Se crea el componente "ChatSuggestions" que renderiza un carrusel de opciones predefinidas utilizando la librería "embla-carousel-react".
    El componente recide un array de opciones y devuelve la opción seleccionada.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,6 +7,7 @@ import useGrist from './hooks/useGrist';
 import { generateCompletion } from './services/aiService';
 import type { TGristRow } from './types/TGrist';
 import { getMarkdownTable, extractTableData } from './utils/markdown';
+import ChatSuggestions from './components/ChatSuggestions';
 
 export default function App() {
     const [isGenerating, setIsGenerating] = useState(false)
@@ -51,9 +52,12 @@ export default function App() {
     }
 
     return (
-        <div className='flex flex-col h-full'>
-            <section className='flex-1 overflow-y-auto pb-4 mask-b-from-[calc(100%-1rem)] mask-b-to-100%'>
+        <div className='flex flex-col h-full gap-2'>
+            <section className='flex-1 overflow-y-auto pb-2 mask-b-from-[calc(100%-1rem)] mask-b-to-100%'>
                 <ChatList messages={messages}/>
+            </section>
+            <section>
+                <ChatSuggestions options={['Análisis y Resumen', 'Traducir texto', 'Generar contenido', 'Modificar contenido']} onSelect={(option) => console.log(option)}/>
             </section>
             <section className='p-2 border-2 border-neutral-200 rounded-xl shadow-md'>
                 <MessageInput disabled={isGenerating} onSend={(message, context, model) => sendMessage(message, context, model)}/>
