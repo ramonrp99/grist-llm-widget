78d44ac5e8d45b692013c946cc1227b72a123f93
ramonrp99 <ramonrodped@gmail.com>
2026-03-20 20:30:18 +0100

    [feat] Añade comunicación con Ollama en /chat
    - Se mueve el cacheo del listado de modelos de "aiService" a "aiController" y se modifica para que incluya también los modelos locales.
    - Se añade función "generateOllamaCompletion" en "aiService", encargada de realizar las peticiones a los modelos de Ollama.
    - Se añade lógica para comprobar el tipo de modelo seleccionado a la función"generateCompletion" en "aiController". Si el modelo es "local" llama a "generateOllamaCompletion", si no, llama a "generateOpenRouterCompletion".

--- backend/src/services/aiService.js
+++ backend/src/services/aiService.js
@@ -1,8 +1,6 @@
 const config = require('../config/env')
 const AppError = require('../core/AppError')
 
-let cachedModels = null
-
 // Obtener listado de modelos disponibles en OpenRouter
 const getOpenRouterModels = async () => {
     const res = await fetch('https://openrouter.ai/api/v1/models', {
@@ -21,29 +19,7 @@ const getOpenRouterModels = async () => {
         throw new AppError(statusCode, message)
     }
 
-    cachedModels = json.data
-
     return json.data
-}
-
-// Obtener listado de modelos locales disponibles
-const getLocalModels = async () => {
-
-}
-
-// Obtener listado de modelos disponibles
-const getAvailableModels = async () => {
-    if(!cachedModels) {
-        return await getOpenRouterModels()
-    }
-
-    return cachedModels
-}
-
-const getModelById = async (modelId) => {
-    const models = await getAvailableModels()
-
-    return models.find(m => m.id === modelId)
 }
 
 // Enviar prompt a modelo de OpenRouter
@@ -72,14 +48,29 @@ const generateOpenRouterCompletion = async (model, messages) => {
     return json.choices[0].message.content
 }
 
-// Enviar prompt a modelo local
-const generateLocalCompletion = async (data) => {
+// Enviar prompt a modelo de Ollama
+const generateOllamaCompletion = async (url, model, messages) => {
+    const res = await fetch(`${url}/api/chat`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            'model': model,
+            'messages': messages
+        })
+    })
 
+    const json = await res.json()
+
+    if(!res.ok || json.error) {
+        const message = json.error.message || 'Error al generar una respuesta con Ollama'
+        const statusCode = res.status || 500
+
+        throw new AppError(statusCode, message)
+    }
+
+    return json.message.content
 }
 
-// Enviar prompt al modelo seleccionado
-const generateCompletion = async (model, messages) => {
-    return await generateOpenRouterCompletion(model, messages)
-}
-
-module.exports = { getAvailableModels, getModelById, generateCompletion }
+module.exports = { getOpenRouterModels, generateOpenRouterCompletion, generateOllamaCompletion }
