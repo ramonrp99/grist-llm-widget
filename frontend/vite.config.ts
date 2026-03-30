64426b2671df8b3f293374d177e00f68364b70d8
ramonrp99 <ramonrodped@gmail.com>
2026-03-30 20:40:38 +0200

    [test] Crea pruebas unitarias widget para utils y config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,6 +1,8 @@
+/// <reference types="vitest/config" />
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -8,4 +10,19 @@ export default defineConfig({
     react(),
     tailwindcss()
   ],
+  resolve: {
+    alias: {
+      '@components': path.resolve(__dirname, './src/components'),
+      '@config': path.resolve(__dirname, './src/config'),
+      '@hooks': path.resolve(__dirname, './src/hooks'),
+      '@customTypes': path.resolve(__dirname, './src/types'),
+      '@utils': path.resolve(__dirname, './src/utils')
+    }
+  },
+  test: {
+    environment: 'jsdom',
+    globals: true,
+    setupFiles: './tests/unit/setupUnitTests.ts',
+    reporters: ['verbose']
+  }
 })
